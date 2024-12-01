07761b320ff2aa717ba1359aa2caa457e08688a3
Ian-Stentz <ianstentz25@gmail.com>
2024-11-30 16:52:54 -0800

    Renamed assets to standard convention, progress on updating to phaser scenes and sprites
    * Not good to work, I'm commiting to move from laptop to desktop

--- docs/src/Scenes/Farm.js
+++ docs/src/Scenes/Farm.js
@@ -9,6 +9,7 @@ class Farm extends Phaser.Scene {
 
     init() {
         //any initialization of global variables go here
+        this.board = new Board(6, 6);
     }
 
     create() {
@@ -19,5 +20,30 @@ class Farm extends Phaser.Scene {
         //update function goes here
     }
 
+    draw() {
+        this.drawBoard();
+    }
+
     //helper functions go here
+    drawBoard() {
+        let width = game.config.width/this.board.width;
+        let height = game.config.height/this.board.height;
+        for (let i = 0; i < this.board.width; i++) {
+            for (let j = 0; j < this.board.height; j++) {
+                stroke("black");
+                strokeWeight(3);
+                //in future, fill could change given certain factors or be replaced with a sprite/tile
+                fill("#118c13");
+                rect(i*width, j*height, width, height);
+                stroke("white");
+                text("☀️: " + this.board.getEntry(i, j).sunlight, i*width + width/12, j*height + height/8);
+                text("💧: " + this.board.getEntry(i, j).moisture, i*width + width/12, j*height + height/3);
+                if(this.board.getEntry(i, j)["crop"] != null) {
+                //fill("#1e833b");
+                //rect(i*width+3*width/5, j*height+height/2,width/5, height/3);
+                //drawPlant(board, i, j);
+                }
+            }
+        }
+    }
 }
--- docs/src/Scenes/Load.js
+++ docs/src/Scenes/Load.js
@@ -4,11 +4,48 @@ class Load extends Phaser.Scene {
     }
 
     preload() {
-        //any loading not done in Load.js goes here
+        this.load.setPath("./assets/");
+        this.load.image("Player", "player.png");
+
+        
     }
 
     create() {
         //creation of world objects goes here
+
+        // Testing if this can automate (ish) crop sprites
+        // this.anims.create({
+        //     key: "plantA",
+        //     frames: this.anims.generateFrameNames("crop", {
+        //         prefix: "plantA-",
+        //         start: 0,
+        //         end: 3,
+        //         suffix: ".png"
+        //     }),
+        //     frameRate: 0,
+        // });
+        // this.anims.create({
+        //     key: "plantB",
+        //     frames: this.anims.generateFrameNames("crop", {
+        //         prefix: "plantB-",
+        //         start: 0,
+        //         end: 3,
+        //         suffix: ".png"
+        //     }),
+        //     frameRate: 0,
+        // });
+        // this.anims.create({
+        //     key: "plantC",
+        //     frames: this.anims.generateFrameNames("crop", {
+        //         prefix: "plantC-",
+        //         start: 0,
+        //         end: 2,
+        //         suffix: ".png"
+        //     }),
+        //     frameRate: 0,
+        // });
+
+        //go to the farm scene
         this.scene.start("farmScene");
     }
 
