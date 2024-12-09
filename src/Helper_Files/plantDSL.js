ae6f46ef111b4d5f1962cbd5a5bedae646ed2f4a
Carolyn-Hope256 <cmhope@ucsc.edu>
2024-12-08 19:47:22 -0800

    Added domain-specific language for adding crop types. Still needs to be implemented. Added programmer art for ponds.

---
+++ src/Helper_Files/plantDSL.js
@@ -0,0 +1,127 @@
+
+class PlantType{
+    id;
+    plantName;
+
+    stages;
+    sprites;
+
+    sunReq;
+    waterReq;
+    waterCons;
+
+    adjacencies;
+    parents;
+
+    unlocked;
+    constructor(){
+        this.id = -1;
+        this.plantName = "Plant";
+        
+        this.stages = 4;
+        this.sprites = [{stage: 0, sprite: "sprite1"}, {stage: 3, sprite: "sprite2"}];
+
+        this.sunReq = 9;
+        this.waterReq = 20;
+        this.waterCons = 10;
+
+        this.adjacencies = []; // contains object of format {crop: string, bonus: number}
+        this.parents = [];
+        this.unlocked = false;
+    };
+
+    isStarter(){
+        return(this.parents.length < 1);
+    }
+
+    canCrossbreed(adjacent){
+        if(this.isStarter()){ //starter crops can't be crossbred
+            return(false);
+        }
+
+        for(let i in this.parents){
+            let hasParent = false;
+            for(let j in adjacent){
+                if(i == j){
+                    hasParent = true;
+                    break;
+                }
+            }
+            if(!hasParent){
+                return(false);
+            }
+        }
+
+        return(true);
+    }
+
+    canGrow(sun, water, stage, adjacent){ //if crop can't grow, returns -1, otherwise returns water cost
+        const c = this.getCoeff(adjacent); //grabs the adjacency coefficient that multiplies all requirements
+
+        if(this.sunReq * c < sun && this.waterReq * c < water && stage < this.stages - 1){
+            return(this.waterCons * c);
+        }
+        return(-1);
+    }
+
+    getCoeff(adjacent){ 
+        let c = 1;
+
+        for(let i = 0; i < adjacencies.length; i++){
+            for(let j = 0; j < adjacent.length; j++){
+                if(this.adjacencies[i].crop == this.adjacent[j]){
+                    c /= this.adjacencies[i].bonus;
+                }
+            }
+        }
+        return(c);
+    }
+
+    getSprite(stage){
+        let outSprite = "";
+
+        for(let s in this.sprites){
+            if(stage >= s.stage){
+                outSprite = s.sprite;
+            }
+        }
+
+        return(outSprite);
+    }
+
+}
+
+function plantCompiler(plantProgram, sprites){
+    let newPlant = new PlantType();
+
+    const plantLang = {
+        ID: function(id){
+            newPlant.id = id;
+        },
+        Name: function(n){
+            newPlant.plantName = n;
+        },
+        Stages: function(stages){
+            newPlant.stages = stages;
+        },
+        SunNeeded: function(sun){
+            newPlant.sunReq = sun;
+        },
+        WaterNeeded: function(water){
+            newPlant.waterReq = water;
+        },
+        WaterConsumed: function(water){
+            newPlant.waterCons = water;
+        },
+        Adjacencies: function(adj){
+            newPlant.adjacencies = adj;
+        },
+        Parents: function(parents){
+            newPlant.parents = parents;
+        }
+    }
+
+    plantProgram(plantLang);
+    return(newPlant);
+}
+
